304a23c4a63ce09b68af8b77e0b8076f40e050ac
Abhishek Singh <abhishek.abhi90@gmail.com>
2023-09-25 02:41:15 +0530

    Changes

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu + Umang + Abhishek!";
+		return "Greetings from Tanzu + Umang + Abhishek!";
 	}
 
 }
